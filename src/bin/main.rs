d3abe482f174239997b6c51e043f7ea55eee27cd
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-30 22:24:27 -0400

    wip

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused)]
 use ai::{prelude::*, tui};
+use ratatui::{layout::Alignment, symbols::border, text::Text, widgets::Borders};
 use std::{io, panic::catch_unwind};
 
 #[derive(Debug, thiserror::Error)]
@@ -37,9 +38,40 @@ impl App {
         Ok(())
     }
     fn render_frame(&self, frame: &mut Frame) {
-        todo!()
+        frame.render_widget(self, frame.size())
     }
     fn handle_events(&mut self) -> Result<(), Error> {
         todo!()
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let title = Title::from(" Counter App Tutorial".bold());
+        let instructions = Title::from(Line::from(vec![
+            " Decrement ".into(),
+            "<Left>".blue().bold(),
+            " Increment ".into(),
+            "<Right>".blue().bold(),
+            " Quit ".into(),
+            "<Q>".blue().bold(),
+        ]));
+        let block = Block::default()
+            .title(title.alignment(Alignment::Center))
+            .title(
+                instructions
+                    .alignment(Alignment::Center)
+                    .position(ratatui::widgets::block::Position::Bottom),
+            )
+            .borders(Borders::ALL)
+            .border_set(border::THICK);
+        let counter_text = Text::from(vec![Line::from(vec![
+            "Value".into(),
+            self.counter.to_string().yellow(),
+        ])]);
+        Paragraph::new(counter_text)
+            .centered()
+            .block(block)
+            .render(area, buf);
     }
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -4,10 +4,16 @@ pub use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
+pub use ratatui::prelude::Buffer;
+pub use ratatui::widgets::Block;
 pub use ratatui::{
     prelude::{CrosstermBackend, Rect, Stylize, Terminal},
     widgets::Paragraph,
     Frame,
+};
+pub use ratatui::{
+    text::Line,
+    widgets::{block::Title, Widget},
 };
 pub use std::io::stdout;
 pub use std::{error::Error, io::Stdout, time::Duration};
