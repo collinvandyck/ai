cdbc1b363862267073587922b8b486d315d2a4bb
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-26 21:27:52 -0500

    wip

--- examples/test-api.rs
+++ examples/test-api.rs
@@ -8,7 +8,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
     ai::tracing::init();
     let key = env::var("ANTHROPIC_API_KEY").context("no api key")?;
     let client = ai::anthropic::Client::new(key).context("new client")?;
-    let resp = client.speak("test test test").await.context("speak")?;
+    let resp = client
+        .speak("any one sentence tips for writing an anthropic rust client?")
+        .await
+        .context("speak")?;
     info!("Response: {resp:#?}");
     Ok(())
 }
