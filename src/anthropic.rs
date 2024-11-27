1f0f3dedebb9c2132481a9128f0ff82a5cd90eba
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-26 20:59:21 -0500

    wip

--- src/anthropic.rs
+++ src/anthropic.rs
@@ -1,13 +1,13 @@
-use std::{sync::Arc, time::Duration};
+use std::{str::FromStr, sync::Arc, time::Duration};
 
 use anyhow::{Context, Result};
-use reqwest::RequestBuilder;
+use reqwest::{Method, RequestBuilder};
 use serde::{Deserialize, Serialize};
 
 pub struct Client {
     key: String,
-    endpoint: String,
-    model: Arc<String>,
+    endpoint: url::Url,
+    model: String,
     version: String,
     max_tokens: u32,
     client: reqwest::Client,
@@ -15,8 +15,8 @@ pub struct Client {
 
 impl Client {
     pub fn new(key: String) -> Result<Self> {
-        let endpoint = String::from("https://api.anthropic.com");
-        let model = Arc::new(String::from("claude-3-5-sonnet-20241022"));
+        let endpoint = url::Url::parse("https://api.anthropic.com/").context("parse endpoint")?;
+        let model = String::from("claude-3-5-sonnet-20241022");
         let version = String::from("2023-06-01");
         let max_tokens = 1024;
         let client = reqwest::ClientBuilder::default()
@@ -34,7 +34,24 @@ impl Client {
     }
 
     async fn speak(&self, msg: &str) -> Result<MessagesResponse> {
-        todo!()
+        let method = reqwest::Method::POST;
+        let url = self.endpoint.join("/v1/messages").context("build url")?;
+        let body = MessagesRequest {
+            model: self.model.clone(),
+            max_tokens: self.max_tokens,
+            messages: vec![Message {
+                role: String::from("user"),
+                content: msg.to_string(),
+            }],
+        };
+        let req = self
+            .new_request(method, url)
+            .json(&body)
+            .build()
+            .context("build request")?;
+        let resp = self.client.execute(req).await.context("exec req")?;
+        let resp = resp.json::<MessagesResponse>().await?;
+        Ok(resp)
     }
 
     fn new_request(&self, method: reqwest::Method, url: impl reqwest::IntoUrl) -> RequestBuilder {
