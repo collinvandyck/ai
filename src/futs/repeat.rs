88d54d99d40f23815da29f0036978bd190c25381
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-27 19:07:27 -0500

    wip

--- src/futs/repeat.rs
+++ src/futs/repeat.rs
@@ -17,7 +17,14 @@ where
     F: Fn() -> Fut,
     Fut: Future<Output = Item> + Unpin,
 {
-    fn new(f: F) -> impl Stream<Item = Item> {
+    fn new(f: F) -> Self {
+        Self {
+            f: Box::new(f),
+            state: RepeatState::Empty,
+        }
+    }
+
+    fn from_closure(f: F) -> impl Stream<Item = Item> {
         Self {
             f: Box::new(f),
             state: RepeatState::Empty,
@@ -93,7 +100,50 @@ where
 mod repeat_test {
     use super::*;
     use futures::StreamExt;
-    use tokio::pin;
+    use tokio::{pin, sync::Mutex};
+
+    #[test]
+    fn test_arc() {
+        //
+        let val = Arc::new(std::sync::Mutex::new(42));
+        let f1 = || {
+            let mut val = val.lock().unwrap();
+            *val += 1;
+            *val
+        };
+        let f2 = || {
+            let mut val = val.lock().unwrap();
+            *val += 1;
+            *val
+        };
+        assert_eq!(f1(), 43);
+        assert_eq!(f2(), 44);
+        assert_eq!(f1(), 45);
+    }
+
+    #[tokio::test]
+    async fn repeat_mut() {
+        let val = Arc::new(Mutex::new(42));
+        let mut s = Repeat::new(|| {
+            Box::pin(async {
+                let mut val = val.lock().await;
+                *val += 1;
+                *val
+            })
+        });
+        assert_eq!(s.next().await.unwrap(), 43);
+        assert_eq!(s.next().await.unwrap(), 44);
+
+        let mut s = Repeat::new(|| {
+            Box::pin(async {
+                let mut val = val.lock().await;
+                *val += 1;
+                *val
+            })
+        });
+        assert_eq!(s.next().await.unwrap(), 45);
+        assert_eq!(s.next().await.unwrap(), 46);
+    }
 
     #[tokio::test]
     async fn repeat_test() {
